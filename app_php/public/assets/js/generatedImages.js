93bc4c9acb27b1eecb377a3a488fc2d0fbfc1425
rogeriesmontsia <rogerarques@iesmontsia.org>
2023-11-17 17:27:09 +0100

    fake-prompts v0.2

--- app_php/public/assets/js/generatedImages.js
+++ app_php/public/assets/js/generatedImages.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $("#contentAfterLoading").hide();
     $("#spinner").hide();
     $("#img1").hide();
     $("#img2").hide();
@@ -11,14 +12,18 @@ $(document).ready(function () {
         $("#textPrompt").hide();
         $("#promptButton").hide();
         $("#spinner").show();
+        $("#contentAfterLoading").show();
         setTimeout(function () {
-            $("#spinner").hide();
-            $("#img1").fadeIn(1000, function () {
-                $("#img2").fadeIn(1000, function () {
-                    $("#img3").fadeIn(1000, function () {
-                        $("#img4").fadeIn(1000, function () {
-                            $("#continueButton").fadeIn(1000, function () {
-                                $("#keepTrying").fadeIn(3000);
+            $("#spinner").fadeOut(3000, function () {
+
+
+                $("#img1").fadeIn(1000, function () {
+                    $("#img2").fadeIn(1000, function () {
+                        $("#img3").fadeIn(1000, function () {
+                            $("#img4").fadeIn(1000, function () {
+                                $("#continueButton").fadeIn(1000, function () {
+                                    $("#keepTrying").fadeIn(3000);
+                                });
                             });
                         });
                     });
--- app_php/views/generateImages.php
+++ app_php/views/generateImages.php
@@ -14,41 +14,44 @@
                     <input type="text" class="form-control" id="textPrompt" name="prompt">
                     <button id="promptButton" type="button" class="btn btn-primary mt-3 w-50" name="promptButton">Generar imatges</button>
                 </div>
-                <div class="row justify-content-center">
-                    <div id="spinner" class="spinner-grow text-light mt-3" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                    </div>
-                </div>
+
             </div>
 
         </div>
     </div>
     </div>
-    <div id="contentAfterLoading" class="container w-50 mt-3 text-center">
-        <div class="row">
-            <div class="col">
-                <img src="../public/assets/img/generatedImages/image-1.jpeg" id="img1" class="rounded img-fade" alt="..." width="200" height="200">
+    <div id="contentAfterLoading" class="container w-50 modal-content rounded p-3 p-md-5 text-center bg-success">
+        <div class="row justify-content-center">
+            <div id="spinner" class="spinner-grow text-light mt-3" role="status">
+                <span class="visually-hidden">Loading...</span>
             </div>
-            <div class="col">
-                <img src="../public/assets/img/generatedImages/image-2.jpeg" id="img2" class="rounded img-fade" alt="..." width="200" height="200">
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 mb-3 mb-md-0">
+                <img src="../public/assets/img/generatedImages/image-1.jpeg" id="img1" class="rounded img-fade" alt="..." width="70%" height="auto">
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="../public/assets/img/generatedImages/image-2.jpeg" id="img2" class="rounded img-fade" alt="..." width="70%" height="auto">
             </div>
         </div>
         <div class="row mt-3">
-            <div class="col">
-                <img src="../public/assets/img/generatedImages/image-3.jpeg" id="img3" class="rounded img-fade" alt="..." width="200" height="200">
+            <div class="col-12 col-md-6 mb-3 mb-md-0">
+                <img src="../public/assets/img/generatedImages/image-3.jpeg" id="img3" class="rounded img-fade" alt="..." width="70%" height="auto">
             </div>
-            <div class="col">
-                <img src="../public/assets/img/generatedImages/image-4.jpeg" id="img4" class="rounded img-fade" alt="..." width="200" height="200">
-            </div>
-        </div>
-        <div class="col">
-            <div class="row">
-                <button id="continueButton" type="button" class="btn btn-primary mt-3" name="continueButton">Escollir un suport</button>
-            </div>
-            <div class="row">
-                <button id="keepTrying" type="button" class="btn btn-primary mt-3" name="keepTrying">Torna a generar una imatge</button>
+            <div class="col-12 col-md-6">
+                <img src="../public/assets/img/generatedImages/image-4.jpeg" id="img4" class="rounded img-fade" alt="..." width="70%" height="auto">
             </div>
         </div>
+    </div>
+
+    <div class="col">
+        <div class="row justify-content-center">
+            <button id="continueButton" type="button" class="btn btn-primary mt-3 w-25" name="continueButton">Escollir un suport</button>
+        </div>
+        <div class="row justify-content-center">
+            <button id="keepTrying" type="button" class="btn btn-primary mt-3 w-25" name="keepTrying">Torna a generar una imatge</button>
+        </div>
+    </div>
 </body>
 
 
