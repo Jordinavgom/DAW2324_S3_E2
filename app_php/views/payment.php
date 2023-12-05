aac03e6d41547327b36bef077a470a4086ddc666
rogeriesmontsia <rogerarques@iesmontsia.org>
2023-12-05 18:52:42 +0100

    arreglar pagina pagament

--- app_php/views/payment.php
+++ app_php/views/payment.php
@@ -14,46 +14,42 @@
         <div id="cards">
           <img src="http://icons.iconarchive.com/icons/designbolts/credit-card-payment/256/Visa-icon.png">
           <img src="http://icons.iconarchive.com/icons/designbolts/credit-card-payment/256/Master-Card-icon.png">
-        </div><!--#cards end-->
-      </div><!--col-xs-5 end-->
-      <div class="col-xs-5">
-        <!--#cards end-->
-      </div><!--col-xs-5 end-->
-    </div><!--row end-->
-    <div class="row">
-      <div class="alert alert-danger" id="generalAlert" role="alert">Completa tots els camps</div>
-      <div class="col-xs-5">
-        <i class="fa fa fa-user"></i>
-        <label for="cardholder">Nom del titular de la targeta</label>
-        <input type="text" placeholder="John Doe" id="cardholder">
-        <!-- <div class="alert alert-danger" id="alert-cardholder" role="alert"></div> -->
-      </div><!--col-xs-5-->
-      <div class="col-xs-5">
-        <i class="fa fa-credit-card-alt"></i>
-        <label for="cardnumber">Número de la targeta</label>
-        <input type="text" placeholder="1234 1234 1234 1234" id="cardnumber">
-        <!-- <div class="alert alert-danger" id="alertmail" role="alert"></div> -->
-      </div><!--col-xs-5-->
-    </div><!--row end-->
-    <div class="row row-three">
-      <div class="col-xs-2">
-        <i class="fa fa-calendar"></i>
-        <label for="date">Valid fins</label>
-        <input type="text" placeholder="MM/YY" id="date">
-        <!-- <div class="alert alert-danger" id="alertmail" role="alert"></div> -->
-      </div><!--col-xs-3-->
-      <div class="col-xs-2">
-        <i class="fa fa-lock"></i>
-        <label for="date">CVV / CVC *</label>
-        <input type="text" id="cvc" placeholder="000">
-        <!-- <div class="alert alert-danger" id="alertmail" role="alert"></div> -->
-      </div><!--col-xs-3-->
-      <!-- <div class="col-xs-5">
-        <span class="small">* CVV or CVC is the card security code, unique three digits number on the back of your card seperate from its number.</span>
-      </div>col-xs-6 end -->
-    </div><!--row end-->
+        </div>
+      </div>
+
+    </div>
+
+    <div class="alert alert-danger" id="generalAlert" role="alert">Completa tots els camps</div>
+    <div class="row w-50 mx-auto">
+      <div class="col">
+        <div class="form-group mb-3">
+          <i class="fa fa-user"></i>
+          <label for="cardholder">Nom del titular de la targeta</label>
+          <input type="text" class="form-control" placeholder="John Doe" id="cardholder">
+        </div>
+
+        <div class="form-group mb-3">
+          <i class="fa fa-credit-card-alt"></i>
+          <label for="cardnumber">Número de la targeta</label>
+          <input type="text" class="form-control" placeholder="1234 1234 1234 1234" id="cardnumber">
+        </div>
+
+        <div class="form-group mb-3">
+          <i class="fa fa-calendar"></i>
+          <label for="date">Valid fins</label>
+          <input type="text" class="form-control" placeholder="MM/YY" id="date">
+        </div>
+
+        <div class="form-group mb-3">
+          <i class="fa fa-lock"></i>
+          <label for="cvc">CVV / CVC *</label>
+          <input type="text" class="form-control" id="cvc" placeholder="000">
+        </div>
+      </div>
+    </div>
+
     <footer class="text-center">
-      <button class="btn btn-success" id="boto-pagar">Pagar</a>
+      <button class="btn btn-success" id="boto-pagar" onclick=comprovaDades()>Pagar</a>
     </footer>
-  </div><!--wrapper end-->
+  </div>
 </body>
