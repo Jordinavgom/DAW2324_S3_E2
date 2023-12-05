b4618b8242db48a251936247f6334356d07d14df
rogeriesmontsia <rogerarques@iesmontsia.org>
2023-12-05 17:10:51 +0100

    arreglar estils

--- app_php/views/shopping_cart.php
+++ app_php/views/shopping_cart.php
@@ -13,105 +13,111 @@ include('header.php');
     <script src="../public/assets/js/updateUser.js"></script>
 </head>
 
-<div class='container'>
-    <div class='window'>
-        <div class='order-info'>
-            <div class='order-info-content'>
-                <h2>Carret de compres</h2>
-                <div class='line'></div>
-                <table class='order-table'>
-                    <thead class="text-center">
-                        <tr>
-                            <th>Producte</th>
-                            <th>Descripció</th>
-                            <th>Imatge generada</th>
-                            <th>Quantitat</th>
-                            <th>Eliminar</th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-center">
-                        <?php foreach ($orders as $order) : ?>
+<body>
+    <div class='container' style="justify-content: center;">
+        <div class='window modal-content'>
+            <div class='order-info'>
+                <div class='order-info-content'>
+                    <h2 style="color: white;">Carret de compres</h2>
+                    <div class='line'></div>
+                    <table class='order-table'>
+                        <thead class="text-center">
                             <tr>
-                                <td>
-                                    <?php echo '<img src="data:image/jpeg;base64,' . base64_encode($order['image']) . '" class="full-width" />'; ?>
-                                    <!-- <img src='https://dl.dropboxusercontent.com/s/sim84r2xfedj99n/%24_32.JPG' class='full-width'></img> -->
-                                </td>
-                                <td><?= $order['name']; ?></td>
-                                <td>
-                                    <?php echo '<img src="data:image/jpeg;base64,' . base64_encode($order['generatedImage']) . '" style="width: 150px; height: 150px;"/>'; ?>
-                                    <!-- <img src='https://dl.dropboxusercontent.com/s/sim84r2xfedj99n/%24_32.JPG' class='full-width'></img> -->
-                                </td>
-                                <td><?= $order['quantity']; ?></td>
-                                <td><button class="btn btn-danger">Eliminar</button></td>
+                                <th>Producte</th>
+                                <th>Descripció</th>
+                                <th>Imatge generada</th>
+                                <th>Quantitat</th>
+                                <th>Eliminar</th>
                             </tr>
-                            <tr>
-                                <td>
-                                    <div class='price'><?= $order['priceEach']; ?>€</div>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
+                        </thead>
+                        <tbody class="text-center">
+                            <?php foreach ($orders as $order) : ?>
+                                <tr>
+                                    <td>
+                                        <?php echo '<img src="data:image/jpeg;base64,' . base64_encode($order['image']) . '" class="full-width" />'; ?>
+                                        <!-- <img src='https://dl.dropboxusercontent.com/s/sim84r2xfedj99n/%24_32.JPG' class='full-width'></img> -->
+                                    </td>
+                                    <td><?= $order['name']; ?></td>
+                                    <td>
+                                        <?php echo '<img src="data:image/jpeg;base64,' . base64_encode($order['generatedImage']) . '" style="width: 150px; height: 150px;"/>'; ?>
+                                        <!-- <img src='https://dl.dropboxusercontent.com/s/sim84r2xfedj99n/%24_32.JPG' class='full-width'></img> -->
+                                    </td>
+                                    <td><?= $order['quantity']; ?></td>
+                                    <td><button class="btn btn-danger">Eliminar</button></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class='price' style="color:white"><?= $order['priceEach']; ?>€</div>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
 
-                </table>
-                <div class='line'></div>
-                <div class='total'>
-                    <span style='float:left;'>
-                        TOTAL <?= $order['priceEach']; ?>€
-                    </span>
-                    <span style='float:right; text-align:right;'>
-                        <button id="boto-pagament" class="btn btn-primary">Procedir al pagament</button>
-                    </span>
+                    </table>
+                    <div class='line'></div>
+                    <div class='total'>
+                        <span style='float:left; color:white;'>
+                            TOTAL <?= $order['priceEach']; ?>€
+                        </span>
+                        <span style='float:right; text-align:right;'>
+                            <button id="boto-pagament" class="btn btn-primary">Procedir al pagament</button>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div id="updateDetails" class='container' style="justify-content: center;">
+        <div class='window p-5'>
+            <form class id="formulario" action="../controllers/UserController.php?action=update" method="post" style="margin-top: 20px;">
+                <h2>Dades per a l'enviament</h2>
+                <div class="row">
+                    <div class="col-md-4">
+                        <label for="nom" class="form-label">Nom</label>
+                        <input type="text" class="form-control" id="nom" name="nom">
+                        <div class="alert alert-danger" id="alertNom" role="alert"></div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <label for="cognoms" class="form-label">Cognoms</label>
+                        <input type="text" class="form-control" id="cognoms" name="cognoms">
+                        <div class="alert alert-danger" id="alertCognoms" role="alert"></div>
+                    </div>
 
 
-<div id="updateDetails" class='container'>
-    <div class='window'>
-        <form class id="formulario" action="../controllers/UserController.php?action=update" method="post" style="margin-top: 20px;">
-            <div class="row">
-                <div class="col-md-4">
-                    <label for="nom" class="form-label">Nom</label>
-                    <input type="text" class="form-control" id="nom" name="nom">
-                    <div class="alert alert-danger" id="alertNom" role="alert"></div>
+                    <div class="col-md-4">
+                        <label for="adreça" class="form-label">Adreça</label>
+                        <input type="text" class="form-control" id="adreça" name="adreça">
+                        <div class="alert alert-danger" id="alertAdreça" role="alert"></div>
+                    </div>
+
                 </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <label for="ciutat" class="form-label">Ciutat</label>
+                        <input type="text" class="form-control" id="ciutat" name="ciutat">
+                        <div class="alert alert-danger" id="alertCiutat" role="alert"></div>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="codipostal" class="form-label">Codi Postal</label>
+                        <input type="text" class="form-control" id="codipostal" name="codipostal">
+                        <div class="alert alert-danger" id="alertCodiPostal" role="alert"></div>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="telefon" class="form-label">Número de telèfon</label>
+                        <input type="text" class="form-control" id="telefon" name="telefon">
+                        <div class="alert alert-danger" id="alertTelefon" role="alert"></div>
+                    </div>
 
-                <div class="col-md-4">
-                    <label for="cognoms" class="form-label">Cognoms</label>
-                    <input type="text" class="form-control" id="cognoms" name="cognoms">
-                    <div class="alert alert-danger" id="alertCognoms" role="alert"></div>
                 </div>
-
-
-                <div class="col-md-4">
-                    <label for="adreça" class="form-label">Adreça</label>
-                    <input type="text" class="form-control" id="adreça" name="adreça">
-                    <div class="alert alert-danger" id="alertAdreça" role="alert"></div>
+                <div class="row justify-content-center">
+                    <button id="boto-update" type="submit" class="btn btn-primary w-50" name="boto-update" style="margin-top:20px;" onclick="formulari()">Procedeix al pagament</button>
                 </div>
-
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <label for="ciutat" class="form-label">Ciutat</label>
-                    <input type="text" class="form-control" id="ciutat" name="ciutat">
-                    <div class="alert alert-danger" id="alertCiutat" role="alert"></div>
-                </div>
-                <div class="col-md-4">
-                    <label for="codipostal" class="form-label">Codi Postal</label>
-                    <input type="text" class="form-control" id="codipostal" name="codipostal">
-                    <div class="alert alert-danger" id="alertCodiPostal" role="alert"></div>
-                </div>
-                <div class="col-md-4">
-                    <label for="telefon" class="form-label">Número de telèfon</label>
-                    <input type="text" class="form-control" id="telefon" name="telefon">
-                    <div class="alert alert-danger" id="alertTelefon" role="alert"></div>
-                </div>
-
-            </div>
-
-            <button id="boto-update" type="submit" class="btn btn-primary" name="boto-update" style="margin-top:20px;" onclick="formulari()">Procedeix al pagament</button>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
+</body>
+
+<?php include('./footer.php');
+?>
