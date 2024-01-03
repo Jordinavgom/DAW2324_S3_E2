a930b9f2f442a055aae86aff9adc7b96756461af
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-01-03 16:48:52 +0100

    afegir logo

--- app_php/index.php
+++ app_php/index.php
@@ -5,19 +5,35 @@ include('views/header.php'); ?>
 
 <body>
     <div class="container">
-        <div class="p-5 mb-4 bg-body-tertiary rounded-3" id="jumbotron">
-            <div class="container-fluid py-5">
-                <h1 class="display-5 fw-bold">Da vida a tus pensamientos</h1>
-                <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one
-                    in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it
-                    to your liking.</p>
-                <a href="./views/generateImages.php">
-                    <button class="btn btn-primary btn-lg" type="button" id="botonCrear">Crear</button>
-                </a>
+        <div class="p-5 bg-body-tertiary rounded-3" id="jumbotron">
+            <div class="row align-items-center">
+                <!-- Columna de imagen -->
+                <div class="col-md-6 order-md-2">
+                    <div class="h-50">
+                        <img src="../public/assets/img/logo.png" class="img-fluid d-block mx-auto" alt="Logo">
+                    </div>
+                </div>
+
+                <!-- Columna de texto -->
+                <div class="col-md-6 order-md-1">
+                    <h1 class="display-5 fw-bold">Da vida a tus pensamientos</h1>
+                    <p class="fs-4">Using a series of utilities, you can create this jumbotron, just like the one
+                        in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it
+                        to your liking.</p>
+
+                    <a href="./views/generateImages.php">
+                        <button class="btn btn-primary btn-lg" type="button" id="botonCrear">Crear</button>
+                    </a>
+                </div>
             </div>
         </div>
+
+        <!-- Título de la sección -->
         <h2 class="tituloSeccion col-md-2">Popular</h2>
     </div>
+
+
+
 
 
 
